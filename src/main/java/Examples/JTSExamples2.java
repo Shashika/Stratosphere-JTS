04c47ad0ac9fdcce924f2b949f4c938c72d99263
Shashika <shashikamuramudalige@gmail.com>
2014-03-19 19:28:30 +0530

    Add new functionalities

--- src/main/java/Examples/JTSExamples2.java
+++ src/main/java/Examples/JTSExamples2.java
@@ -1,5 +1,7 @@
 package Examples;
 
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.*;
 /**
  * Created with IntelliJ IDEA.
  * User: shashika
@@ -8,4 +10,23 @@ package Examples;
  * To change this template use File | Settings | File Templates.
  */
 public class JTSExamples2 {
+
+    public void doJTSOperations(){
+
+         Coordinate[] coordinates =
+               new Coordinate[] {new Coordinate(4, 0), new Coordinate(2, 2),
+                       new Coordinate(4,4), new Coordinate(6, 2), new Coordinate(4, 0) };
+
+       GeometryFactory geometryFactory = new GeometryFactory();
+
+       LinearRing linearRing = geometryFactory.createLinearRing(coordinates);
+       Polygon polygon = geometryFactory.createPolygon(linearRing,null);
+       System.out.println(polygon.getArea());          //Area of the polygon
+
+    }
+
+    public static void main(String[] args) {
+        JTSExamples2 jtsExamples2 = new JTSExamples2();
+        jtsExamples2.doJTSOperations();
+    }
 }
